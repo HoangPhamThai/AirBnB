5c6f877deddd4802c9aeda076f20a6f80a9d7de9
Pham Thai Hoang <thaihoang.pham@masterisehomes.com>
2024-04-14 11:33:14 +0700

    correct role of authgate

--- src/template/AuthGate.jsx
+++ src/template/AuthGate.jsx
@@ -1,11 +1,11 @@
-
 import { appPath } from "../constants/app_path";
 import { userInfoKey, userRole } from "../constants/constants";
 
 export default function AuthGate({ children }) {
   let userInfo = localStorage.getItem(userInfoKey);
   let user = JSON.parse(userInfo);
-  if (!user || user.user.role !== userRole.user) {
+  console.log(user);
+  if (!user || user.user.role !== userRole.admin) {
     window.location.href = appPath.login;
   }
   return children;
